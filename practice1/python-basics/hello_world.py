da85c65a7e96d9b828e12fc8304f74eac58e8cbe
Abibulla Meirzhan <abibullameirzhan@MacBook-Air-Abibulla.local>
2026-01-28 11:21:43 +0500

    Miko

---
+++ practice1/python-basics/hello_world.py
@@ -0,0 +1,20 @@
+#1
+print("Hello, World!")
+
+#2
+if 5 > 2:
+  print("Five is greater than two!")
+
+#3
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#4
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+#5
+print("I am", 35, "years old.")
