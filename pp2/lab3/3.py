0f5f36edf72daf30f2eed8d545f887ceb549ed75
Abibulla Meirzhan <abibullameirzhan@MacBook-Air-Abibulla.local>
2026-02-21 14:39:21 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

--- pp2/lab3/3.py
+++ pp2/lab3/3.py
@@ -1,42 +1,45 @@
-digit = {
-    "ZER": "0","ONE": "1","TWO": "2","THR": "3","FOU": "4","FIV": "5", "SIX": "6", "SEV": "7", "EIG": "8", "NIN": "9"
+w2n = {
+    "ZER": "0", "ONE": "1", "TWO": "2", "THR": "3", "FOU": "4",
+    "FIV": "5", "SIX": "6", "SEV": "7", "EIG": "8", "NIN": "9"
 }
-reversed = {}
-for key in digit:
-    value = digit[key]
-    reversed[value] = key
-def decode(sn):
-    number_as_string = ""
-    index = 0
-    while index < len(sn):
-        t = sn[index:index + 3]
-        number_as_string += digit[t]
-        index += 3
-    return int(number_as_string)
-def encode(number):
-    result = ""
-    for digit in str(number):
-        result += reversed[digit]
-    return result
-ex = input().strip()
-if "+" in ex:
-    parts = ex.split("+")
-    o = "+"
-elif "-" in ex:
-    parts = ex.split("-")
-    o = "-"
-else:
-    parts = ex.split("*")
-    o = "*"
-left_part = parts[0]
-right_part = parts[1]
-left_number = decode(left_part)
-right_number = decode(right_part)
-if o == "+":
-    answer = left_number + right_number
-elif o == "-":
-    answer = left_number - right_number
-else:
-    answer = left_number * right_number
+back_to_word = {"0":"ZER", "1":"ONE", "2":"TWO", "3":"THR", "4":"FOU", "5":"FIV", "6":"SIX", "7":"SEV", "8":"EIG", "9":"NIN"}
 
-print(encode(answer))
+s = input().strip()
+
+if "+" in s:
+    parts = s.split("+")
+    op = "+"
+elif "-" in s:
+    parts = s.split("-")
+    op = "-"
+else:
+    parts = s.split("*")
+    op = "*"
+
+left_str = parts[0]
+num1_str = ""
+for i in range(0, len(left_str), 3):
+    triplet = left_str[i:i+3]
+    num1_str += w2n[triplet]
+num1 = int(num1_str)
+
+right_str = parts[1]
+num2_str = ""
+for i in range(0, len(right_str), 3):
+    triplet = right_str[i:i+3]
+    num2_str += w2n[triplet]
+num2 = int(num2_str)
+
+if op == "+":
+    res = num1 + num2
+elif op == "-":
+    res = num1 - num2
+else:
+    res = num1 * num2
+
+final_res = str(res)
+for digit in final_res:
+    if digit == "-":
+        continue
+    print(back_to_word[digit], end="")
+print()
